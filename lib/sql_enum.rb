98904a7e8d1a213f79c5d8d2a7cd5d04c11f203e
Fletcher Fowler <fletch@fzf.me>
2018-02-28 14:52:52 -0800

    Fixing load order

--- lib/sql_enum.rb
+++ lib/sql_enum.rb
@@ -1,13 +1,5 @@
 require 'active_record'
 
-require_relative 'active_record/enum/enum_type'
-require_relative 'active_record/type/enum'
-require_relative 'active_record/enum_override'
-require_relative 'active_record/connection_adapters/mysql2'
-require_relative 'active_record/connection_adapters/abstract_mysql'
-require_relative 'active_record/connection_adapters/mysql/column_methods'
-require_relative "sql_enum/version"
-
 module SqlEnum
   class << self
     attr_accessor :configuration
@@ -27,3 +19,11 @@ module SqlEnum
     end
   end
 end
+
+require_relative 'active_record/enum/enum_type'
+require_relative 'active_record/type/enum'
+require_relative 'active_record/enum_override'
+require_relative 'active_record/connection_adapters/mysql2'
+require_relative 'active_record/connection_adapters/abstract_mysql'
+require_relative 'active_record/connection_adapters/mysql/column_methods'
+require_relative "sql_enum/version"
