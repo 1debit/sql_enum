aa1374dea02bb4b7d64322b33f5c6841a62cf5d5
Noel Rappin <noel.rappin@chime.com>
2026-02-12 14:22:43 -0600

    first

--- lib/sql_enum/class_methods.rb
+++ lib/sql_enum/class_methods.rb
@@ -13,11 +13,14 @@ module SqlEnum
       suffix = options.fetch(:_suffix, !!SqlEnum.configuration&.default_suffix)
 
       # Define enum using Rails enum
-      enum(column_name => values_map, _prefix: prefix, _suffix: suffix)
+      enum(column_name, values_map, prefix: prefix, suffix: suffix)
 
       # Override reader to return symbols
-      type_definition = ->(subtype) { EnumType.new(attr, send(column_name.to_s.pluralize), subtype) }
-      attribute(column_name, &type_definition)
+      col_name = column_name.to_s
+      decorate_attributes([col_name]) do |_name, subtype|
+        subtype = subtype.subtype if ActiveRecord::Enum::EnumType === subtype
+        EnumType.new(col_name, send(col_name.pluralize), subtype)
+      end
 
       prefix_str = format_affix(column_name, prefix, suffix: '_')
       suffix_str = format_affix(column_name, suffix, prefix: '_')
